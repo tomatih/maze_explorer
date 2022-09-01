0dafcba8334de96c639cd630b40ff8a7e03e88dd
to_matih <maatteeoh@wp.pl>
2022-09-01 23:20:27 +0100

    Added basic movement

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ int main(int argc, char const *argv[])
 	const int screenWidth = 800;
 	const int screenHeigth = 450;
 
+	// game constants
+	const int speed = 5;
+
 	// Initial setup
 	InitWindow(screenWidth, screenHeigth, "Maze Explorer");
 	SetTargetFPS(60);
@@ -20,14 +23,39 @@ int main(int argc, char const *argv[])
 		.projection = CAMERA_PERSPECTIVE,
 	};
 	SetCameraMode(camera, CAMERA_CUSTOM);
+
 	// Game loop
 	while (!WindowShouldClose()) {
+		// Update Stage
+		// movement
+		//TODO: fix higher speed while diagonal
+		if(IsKeyDown(KEY_W)){
+			camera.position.z += speed * GetFrameTime();
+			camera.target.z += speed * GetFrameTime();
+		}
+		if(IsKeyDown(KEY_S)){
+			camera.position.z -= speed * GetFrameTime();
+			camera.target.z -= speed * GetFrameTime();
+		}
+		if(IsKeyDown(KEY_D)){
+			camera.position.x -= speed * GetFrameTime();
+			camera.target.x -= speed * GetFrameTime();
+		}
+		if(IsKeyDown(KEY_A)){
+			camera.position.x += speed * GetFrameTime();
+			camera.target.x += speed * GetFrameTime();
+		}
+
+		UpdateCamera(&camera);
+
+
 		// Drawing stage
 		BeginDrawing();
 			ClearBackground(RAYWHITE); // clean screen
 			BeginMode3D(camera);
 				DrawPlane({0.0f, 0.0f, 0.0f}, {32.0f, 32.0f}, LIGHTGRAY);
 			EndMode3D();
+			DrawFPS(0, 0);
 		EndDrawing();
 	}
 
