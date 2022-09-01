d78d44ea53c34607fe7e3a50997c9bb97f4b4662
to_matih <maatteeoh@wp.pl>
2022-09-01 22:37:32 +0100

    basic skeleton

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
+#include <raylib.h>
+
 int main(int argc, char const *argv[])
 {
-	/* code */
+
+	const int screenWidth = 800;
+	const int screenHeigth = 450;
+
+	InitWindow(screenWidth, screenHeigth, "Maze Explorer");
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+		BeginDrawing();
+			ClearBackground(RAYWHITE);
+			DrawText("Hello World!", 190, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
 	return 0;
 }
