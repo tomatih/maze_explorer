e7865c5a71dc85e30ad95d73f080d70b6a8e51bd
to_matih <maatteeoh@wp.pl>
2022-09-01 22:48:03 +0100

    adding comments

--- main.cpp
+++ main.cpp
@@ -3,20 +3,24 @@
 int main(int argc, char const *argv[])
 {
 
+	// screen constants
 	const int screenWidth = 800;
 	const int screenHeigth = 450;
 
+	// Initial setup
 	InitWindow(screenWidth, screenHeigth, "Maze Explorer");
-
 	SetTargetFPS(60);
 
+	// Game loop
 	while (!WindowShouldClose()) {
+		// Drawing stage
 		BeginDrawing();
 			ClearBackground(RAYWHITE);
 			DrawText("Hello World!", 190, 200, 20, LIGHTGRAY);
 		EndDrawing();
 	}
 
+	// Cleanup
 	CloseWindow();
 
 	return 0;
