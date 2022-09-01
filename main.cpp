3fe1cc3069bbb3973c02e5052f803485c53791d9
to_matih <maatteeoh@wp.pl>
2022-09-01 23:19:32 +0100

    Added 3D camera

--- main.cpp
+++ main.cpp
@@ -11,12 +11,23 @@ int main(int argc, char const *argv[])
 	InitWindow(screenWidth, screenHeigth, "Maze Explorer");
 	SetTargetFPS(60);
 
+	// Camera setup
+	Camera camera = {
+		.position = {0.0f, 1.0f, 0.0f},
+		.target = {0.0f, 1.0f, 1.0f},
+		.up = {0.0f, 1.0f, 0.0f},
+		.fovy = 60.0f,
+		.projection = CAMERA_PERSPECTIVE,
+	};
+	SetCameraMode(camera, CAMERA_CUSTOM);
 	// Game loop
 	while (!WindowShouldClose()) {
 		// Drawing stage
 		BeginDrawing();
-			ClearBackground(RAYWHITE);
-			DrawText("Hello World!", 190, 200, 20, LIGHTGRAY);
+			ClearBackground(RAYWHITE); // clean screen
+			BeginMode3D(camera);
+				DrawPlane({0.0f, 0.0f, 0.0f}, {32.0f, 32.0f}, LIGHTGRAY);
+			EndMode3D();
 		EndDrawing();
 	}
 
