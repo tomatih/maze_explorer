77c02d450f12a1751e1032da7a47eb3f4c1d8ec5
to_matih <maatteeoh@wp.pl>
2022-09-02 00:35:49 +0100

    Player struct creation

--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
 #include <raylib.h>
+#include <raymath.h>
+
+struct Player{
+public:
+	Vector3 position;
+	Quaternion rotation;
+private:
+	float speed;
+	Camera camera;
+	Vector3 camera_postion_offset;
+public:
+	Player(){
+		position = {0.0f, 0.0f,0.0f};
+		speed = 5.0f;
+		camera_postion_offset = {0.0f,1.0f,0.0f};
+		camera = {
+			.position = Vector3Add(position, camera_postion_offset),
+			.target = {0.0f, 1.0f, 1.0f},
+			.up = {0.0f, 1.0f, 0.0f},
+			.fovy = 60.0f,
+			.projection = CAMERA_PERSPECTIVE,
+		};
+		SetCameraMode(camera, CAMERA_CUSTOM);
+	}
+
+	void beginDrawing3D(){
+		BeginMode3D(camera);
+	}
+
+	void update(){
+	}
+};
+
 
 int main(int argc, char const *argv[])
 {
@@ -7,28 +40,22 @@ int main(int argc, char const *argv[])
 	const int screenWidth = 800;
 	const int screenHeigth = 450;
 
+	// Game variables
+	Player player = Player{};
+
 	// Initial setup
 	InitWindow(screenWidth, screenHeigth, "Maze Explorer");
 	SetTargetFPS(60);
 
-	// Camera setup
-	Camera camera = {
-		.position = {0.0f, 1.0f, 0.0f},
-		.target = {0.0f, 1.0f, 1.0f},
-		.up = {0.0f, 1.0f, 0.0f},
-		.fovy = 60.0f,
-		.projection = CAMERA_PERSPECTIVE,
-	};
-	SetCameraMode(camera, CAMERA_CUSTOM);
 
 	// Game loop
 	while (!WindowShouldClose()) {
 		// Update Stage
-		
+		player.update();
 		// Drawing stage
 		BeginDrawing();
 			ClearBackground(RAYWHITE); // clean screen
-			BeginMode3D(camera);
+			player.beginDrawing3D();
 				DrawPlane({0.0f, 0.0f, 0.0f}, {32.0f, 32.0f}, LIGHTGRAY);
 				DrawCube({2.5f,0.5f,0.0f}, 1, 1, 1, RED);
 				DrawCubeWires({2.5f,0.5f,0.0f}, 1, 1, 1, BLACK);
