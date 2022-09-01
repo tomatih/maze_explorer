3c456358fdaa8ffec99250ad399999b0abea5083
to_matih <maatteeoh@wp.pl>
2022-09-01 23:50:37 +0100

    Yea apparently no

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@ int main(int argc, char const *argv[])
 	const int screenWidth = 800;
 	const int screenHeigth = 450;
 
-	// game constants
-	const int speed = 5;
-
 	// Initial setup
 	InitWindow(screenWidth, screenHeigth, "Maze Explorer");
 	SetTargetFPS(60);
@@ -27,33 +24,14 @@ int main(int argc, char const *argv[])
 	// Game loop
 	while (!WindowShouldClose()) {
 		// Update Stage
-		// movement
-		//TODO: fix higher speed while diagonal
-		if(IsKeyDown(KEY_W)){
-			camera.position.z += speed * GetFrameTime();
-			camera.target.z += speed * GetFrameTime();
-		}
-		if(IsKeyDown(KEY_S)){
-			camera.position.z -= speed * GetFrameTime();
-			camera.target.z -= speed * GetFrameTime();
-		}
-		if(IsKeyDown(KEY_D)){
-			camera.position.x -= speed * GetFrameTime();
-			camera.target.x -= speed * GetFrameTime();
-		}
-		if(IsKeyDown(KEY_A)){
-			camera.position.x += speed * GetFrameTime();
-			camera.target.x += speed * GetFrameTime();
-		}
-
-		UpdateCamera(&camera);
-
-
+		
 		// Drawing stage
 		BeginDrawing();
 			ClearBackground(RAYWHITE); // clean screen
 			BeginMode3D(camera);
 				DrawPlane({0.0f, 0.0f, 0.0f}, {32.0f, 32.0f}, LIGHTGRAY);
+				DrawCube({2.5f,0.5f,0.0f}, 1, 1, 1, RED);
+				DrawCubeWires({2.5f,0.5f,0.0f}, 1, 1, 1, BLACK);
 			EndMode3D();
 			DrawFPS(0, 0);
 		EndDrawing();
