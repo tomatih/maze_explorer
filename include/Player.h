10408fa3e48cba8d1e53133f4d296507966619f5
to_matih <maatteeoh@wp.pl>
2022-09-25 18:05:34 +0100

    Player Seperation

---
+++ include/Player.h
@@ -0,0 +1,42 @@
+#include <raylib.h>
+#include <raymath.h>
+
+#include <GameObject.h>
+#include <Maze.h>
+
+#ifndef _PLAYERH_
+#define _PLAYERH_
+
+class Player: public GameObject{
+public:
+	Vector3 position; // player position
+	Camera camera;
+	Maze* maze;
+
+	Player();
+	
+	void Update();
+private:
+	float speed; // movement speed
+	float rotation; // player world orientation
+	float head_rotation; // up/down head rotation
+	float rotation_speed; // head rotation speed
+	Vector3 last_position;
+	//Camera camera; // 3D camera object
+	Vector3 camera_postion_offset; // basically player height
+	Vector3 camera_target_offset; // where player is lookign
+
+	void update_position();
+
+	void update_orientation();
+
+	void update_camera();
+
+	void check_collisions();
+
+	// unvirtualising
+	void Draw2D(); 
+	void Draw3D();
+};
+
+#endif
