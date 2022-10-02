34b213e3ff7ccfd21b7d41bacbf4ea52ec2faf29
to_matih <maatteeoh@wp.pl>
2022-10-02 17:46:35 +0100

    Bug fix

--- src/WelcomeScreen.cpp
+++ src/WelcomeScreen.cpp
@@ -3,13 +3,12 @@
 
 WelcomeScreen::WelcomeScreen(){
 	welcome_text = WelcomeText();
-	welcome_text.manager = manager;
 	game_objects.push_back(&welcome_text);
 	camera3D = nullptr;
 }
 
 void WelcomeScreen::on_enter(){
-
+	welcome_text.manager = manager;
 }
 
 void WelcomeScreen::on_leave(){
